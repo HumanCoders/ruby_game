d7fa8092186bf23fd969691fc4353bc99b4c49e3
Matthieu Segret <matthieu.segret@gmail.com>
2013-03-17 23:44:46 +0100

    Affichage d'une image de fond

--- lib/ruby_game/game.rb
+++ lib/ruby_game/game.rb
@@ -5,12 +5,14 @@ module RubyGame
     def initialize(width, height)
       super(width, height, false)
       self.caption = "Ruby Game"
+      @background_image = Gosu::Image.new(self, File.join(RubyGame::IMAGES_PATH, 'background.png'), true)
     end
 
     def update
     end
 
     def draw
+      @background_image.draw(0, 0, 0)
     end
 
     def start!
