005c2945df76a97e7cba0e40663ced588fb8a332
Matthieu Segret <matthieu.segret@gmail.com>
2013-03-17 23:58:07 +0100

    Affichage du ruby dans la fenêtre

--- lib/ruby_game/game.rb
+++ lib/ruby_game/game.rb
@@ -2,9 +2,12 @@ require 'gosu'
 
 module RubyGame
   class Game < Gosu::Window
-    def initialize(width, height)
+    def initialize(width, height, ruby)
       super(width, height, false)
       self.caption = "Ruby Game"
+      @ruby = ruby
+      @objects = [ruby]
+      @objects.each {|object| object.init_image(self)}
       @background_image = Gosu::Image.new(self, File.join(RubyGame::IMAGES_PATH, 'background.png'), true)
     end
 
@@ -13,6 +16,7 @@ module RubyGame
 
     def draw
       @background_image.draw(0, 0, 0)
+      @objects.each {|object| object.draw}
     end
 
     def start!
---
+++ lib/ruby_game/ruby.rb
@@ -0,0 +1,7 @@
+module RubyGame
+  class Ruby < StaticObject
+    def initialize(x, y, image_name = 'ruby')
+      super
+    end
+  end
+end
