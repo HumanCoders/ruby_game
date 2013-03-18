4f5b392b7b9e3734f1c3103272940568548fd0ed
Matthieu Segret <matthieu.segret@gmail.com>
2013-03-18 17:29:39 +0100

    Création d'un profil de monstre à l'aide d'une DSL

--- lib/ruby_game/monster.rb
+++ lib/ruby_game/monster.rb
@@ -1,15 +1,42 @@
 module RubyGame
   class Monster < MovingObject
 
+    @@monsters = {}
+    Action = Struct.new(:direction, :velocity)
+
     def initialize(x = 0, y = 0, image_name = 'ghost')
       super
       @velocity = 3
+      @actions = []
+    end
+
+    def self.build(name)
+      @@monsters[name].clone
     end
 
     def init_limits(max_width, max_height, border_with, border_top_with)
       super
       @x = rand border_top_with..(max_width - 2*border_top_with)
       @y = rand border_top_with..(max_height - 2*border_top_with)
+    end
+
+    def self.define(name)
+      monster = Monster.new
+      yield(monster)
+      @@monsters[name] = monster
+    end
+
+    def image_name(image_name)
+      @image_name = image_name
+    end
+
+    def velocity(velocity)
+      @velocity = velocity
+    end
+
+    def action(direction, options = {})
+      opts = {velocity: @velocity, repeat: 1}.merge options
+      @actions += Array.new(opts[:repeat]) {Action.new(direction, opts[:velocity])}
     end
   end
 end
