aab73b5773f45a1d190f384a243eba448205ad46
Matthieu Segret <matthieu.segret@gmail.com>
2013-03-19 19:10:07 +0100

    Affichage du ruby dans la fenêtre

---
+++ lib/ruby_game/static_object.rb
@@ -0,0 +1,18 @@
+module RubyGame
+  class StaticObject
+    attr_accessor :x, :y
+
+    def initialize(x, y, image_name)
+      @x, @y = x, y
+      @image_name = image_name
+    end
+
+    def init_image(window)
+      @image = Gosu::Image.new(window, File.join(IMAGES_PATH, "#{@image_name}.png"), true)
+    end
+
+    def draw
+      @image.draw_rot(@x, @y, 1, 0)
+    end
+  end
+end
