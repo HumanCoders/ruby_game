26c606ed4ee0434d88a5e449004f84a67cb9d8a1
Matthieu Segret <matthieu.segret@gmail.com>
2013-03-19 19:05:04 +0100

    Affichage d'une image de fond

--- lib/ruby_game.rb
+++ lib/ruby_game.rb
@@ -2,5 +2,6 @@ require_relative "ruby_game/version"
 require_relative "ruby_game/game"
 
 module RubyGame
-  # Your code goes here...
+  ROOT_PATH = File.expand_path('../', File.dirname(__FILE__))
+  IMAGES_PATH = File.join(ROOT_PATH, '/app/assets/images')
 end
--- lib/ruby_game/game.rb
+++ lib/ruby_game/game.rb
@@ -5,12 +5,14 @@ module RubyGame
     def initialize
       super(640, 480, false)
       self.caption = "Ruby Game"
+      @background_image = Gosu::Image.new(self, File.join(IMAGES_PATH, 'background.png'), true)
     end
 
     def update
     end
 
     def draw
+      @background_image.draw(0, 0, 0)
     end
 
     def start!
