2e3912c4c4e6838b395083b7eb5fa544c78f376e
Matthieu Segret <matthieu.segret@gmail.com>
2015-05-25 15:58:22 +0200

    Le joueur peut le déplacer avec les flèches du clavier

--- lib/ruby_game/game.rb
+++ lib/ruby_game/game.rb
@@ -10,6 +10,13 @@ module RubyGame
       @ruby = Ruby.new(self, 65, 115)
     end
 
+    def update
+      @player.move_left if button_down?(Gosu::Button::KbLeft)
+      @player.move_right if button_down?(Gosu::Button::KbRight)
+      @player.move_up if button_down?(Gosu::Button::KbUp)
+      @player.move_down if button_down?(Gosu::Button::KbDown)
+    end
+
     def draw
       @background_image.draw(0, 0, 0)
       @player.draw
--- lib/ruby_game/player.rb
+++ lib/ruby_game/player.rb
@@ -2,6 +2,23 @@ module RubyGame
   class Player < StaticObject
     def initialize(window, x, y, image_name = 'player')
       super
+      @speed = 3
+    end
+
+    def move_left
+      @x -= @speed
+    end
+
+    def move_right
+      @x += @speed
+    end
+
+    def move_up
+      @y -= @speed
+    end
+
+    def move_down
+      @y += @speed
     end
   end
 end
