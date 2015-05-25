c88eaecaa3bcc57dbaae6db5b31bdef41e23b6e2
Matthieu Segret <matthieu.segret@gmail.com>
2015-05-25 15:41:19 +0200

    Affichage de l'image du joueur

--- lib/ruby_game/game.rb
+++ lib/ruby_game/game.rb
@@ -6,10 +6,12 @@ module RubyGame
       super(640, 480, false)
       self.caption = "Ruby Game"
       @background_image = Gosu::Image.new(self, File.join(IMAGES_PATH, 'background.png'), true)
+      @player_image = Gosu::Image.new(self, File.join(IMAGES_PATH, 'player.png'), true)
     end
 
     def draw
       @background_image.draw(0, 0, 0)
+      @player_image.draw(560, 400, 0)
     end
 
     def start
